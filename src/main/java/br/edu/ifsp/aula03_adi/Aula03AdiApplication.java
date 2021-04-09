9af0fd9a2dbd50714edbd25845e9dc2174f98cef
ZeroLie <fabianaakamine@hotmail.com>
2021-04-09 16:05:12 -0300

    acertando as pastas e o projeto do eclipse

---
+++ src/main/java/br/edu/ifsp/aula03_adi/Aula03AdiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aula03_adi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula03AdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula03AdiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/aula03_adi/Aula03AdiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.aula03_adi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula03AdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
